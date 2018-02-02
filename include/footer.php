3af3bf3251a2687a4dc18f5aa210d94893408c9a
logikaltimesheet <jabumadela86@gmail.com>
2018-02-02 11:52:26 +0200

    include folder

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+	   
+	<footer class="container">
+	
+		<?php 
+			echo "<p>Logikal &copy;</p>".date('Y m ');
+		?>
+	</footer>
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Time Sheet</title>
+
+    <title>calendar</title>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/custom.css">  
+  </head>
+
+  <body class="bg-light">
+
+    <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+      <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="posts.php">All Employee Records</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+   
+
+      
+	  
